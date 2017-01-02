0e9aec1b4cf48ca0a77b37b5d30382ef82e48baf
pcouto <pcouto@terra.com>
2017-01-02 22:18:00 +0100

    Add files via upload

---
+++ testmove/Form1.Designer.vb
@@ -0,0 +1,135 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.ImageList1 = New System.Windows.Forms.ImageList(Me.components)
+        Me.FlowLayoutPanel1 = New System.Windows.Forms.FlowLayoutPanel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ShapeContainer1 = New Microsoft.VisualBasic.PowerPacks.ShapeContainer()
+        Me.RectangleShape1 = New Microsoft.VisualBasic.PowerPacks.RectangleShape()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'ImageList1
+        '
+        Me.ImageList1.ImageStream = CType(resources.GetObject("ImageList1.ImageStream"), System.Windows.Forms.ImageListStreamer)
+        Me.ImageList1.TransparentColor = System.Drawing.Color.Transparent
+        Me.ImageList1.Images.SetKeyName(0, "1920x1080-fondos-de-pantalla-y-fondos-gratis-347.jpg")
+        Me.ImageList1.Images.SetKeyName(1, "globo-sobre-la-playa.jpg")
+        '
+        'FlowLayoutPanel1
+        '
+        Me.FlowLayoutPanel1.AutoScroll = True
+        Me.FlowLayoutPanel1.BackColor = System.Drawing.Color.CadetBlue
+        Me.FlowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown
+        Me.FlowLayoutPanel1.Location = New System.Drawing.Point(39, 34)
+        Me.FlowLayoutPanel1.Name = "FlowLayoutPanel1"
+        Me.FlowLayoutPanel1.Padding = New System.Windows.Forms.Padding(10)
+        Me.FlowLayoutPanel1.Size = New System.Drawing.Size(1255, 592)
+        Me.FlowLayoutPanel1.TabIndex = 6
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(83, 708)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 11
+        Me.Label1.Text = "Label1"
+        '
+        'ShapeContainer1
+        '
+        Me.ShapeContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.ShapeContainer1.Margin = New System.Windows.Forms.Padding(0)
+        Me.ShapeContainer1.Name = "ShapeContainer1"
+        Me.ShapeContainer1.Shapes.AddRange(New Microsoft.VisualBasic.PowerPacks.Shape() {Me.RectangleShape1})
+        Me.ShapeContainer1.Size = New System.Drawing.Size(1344, 730)
+        Me.ShapeContainer1.TabIndex = 12
+        Me.ShapeContainer1.TabStop = False
+        '
+        'RectangleShape1
+        '
+        Me.RectangleShape1.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque
+        Me.RectangleShape1.BorderColor = System.Drawing.SystemColors.ActiveCaption
+        Me.RectangleShape1.Location = New System.Drawing.Point(29, 31)
+        Me.RectangleShape1.Name = "RectangleShape1"
+        Me.RectangleShape1.Size = New System.Drawing.Size(1275, 604)
+        '
+        'Panel1
+        '
+        Me.Panel1.Location = New System.Drawing.Point(142, 657)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(441, 26)
+        Me.Panel1.TabIndex = 13
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(726, 661)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 14
+        Me.Label2.Text = "Label2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(726, 684)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(39, 13)
+        Me.Label3.TabIndex = 15
+        Me.Label3.Text = "Label3"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.CadetBlue
+        Me.ClientSize = New System.Drawing.Size(1344, 730)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.FlowLayoutPanel1)
+        Me.Controls.Add(Me.ShapeContainer1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ImageList1 As ImageList
+    Friend WithEvents FlowLayoutPanel1 As FlowLayoutPanel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ShapeContainer1 As PowerPacks.ShapeContainer
+    Friend WithEvents RectangleShape1 As PowerPacks.RectangleShape
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+End Class
---
+++ testmove/Form1.vb
@@ -0,0 +1,169 @@
+﻿
+
+Imports System.IO
+
+Public Class Form1
+    Public x, y As Integer
+    Public StartPos, Lastpos As Integer
+    Public Mdown As Boolean
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+        Dim PicPath As String = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures)
+        ' *********************************************************************************
+        ' Colocamos un panel sobre el layout para que no se vea la barra de desplazamiento.
+        ' *********************************************************************************
+        Panel1.Left = FlowLayoutPanel1.Left
+        Panel1.Width = FlowLayoutPanel1.Width
+        Panel1.Height = 50
+        Panel1.Top = FlowLayoutPanel1.Top + FlowLayoutPanel1.Height - 50
+        ' *********************************************************************************
+        ' Cargamos 
+        ' *********************************************************************************
+        LoadLayoutImage(PicPath & "\cibaria\1.jpg", 1)
+        LoadLayoutImage(PicPath & "\cibaria\2.jpg", 2)
+        LoadLayoutImage(PicPath & "\cibaria\3.jpg", 3)
+        LoadLayoutImage(PicPath & "\cibaria\4.jpg", 4)
+        LoadLayoutImage(PicPath & "\cibaria\5.jpg", 5)
+        LoadLayoutImage(PicPath & "\cibaria\6.jpg", 6)
+        LoadLayoutImage(PicPath & "\cibaria\7.jpg", 7)
+        LoadLayoutImage(PicPath & "\cibaria\8.jpg", 8)
+        LoadLayoutImage(PicPath & "\cibaria\9.jpg", 9)
+        LoadLayoutImage(PicPath & "\cibaria\10.jpg", 10)
+
+        LoadLayoutImage(PicPath & "\cibaria\12.jpg", 12)
+        LoadLayoutImage(PicPath & "\cibaria\13.jpg", 13)
+        LoadLayoutImage(PicPath & "\cibaria\14.jpg", 14)
+        LoadLayoutImage(PicPath & "\cibaria\15.jpg", 15)
+        LoadLayoutImage(PicPath & "\cibaria\16.jpg", 16)
+        LoadLayoutImage(PicPath & "\cibaria\18.jpg", 18)
+        LoadLayoutImage(PicPath & "\cibaria\17.jpg", 17)
+        LoadLayoutImage(PicPath & "\cibaria\11.jpg", 11)
+
+    End Sub
+
+
+
+
+    Sub PanelMouseDown()
+        Dim MPos As Integer = Cursor.Position.X
+        Lastpos = MPos
+        Mdown = True
+        StartPos = MPos + FlowLayoutPanel1.HorizontalScroll.Value
+
+    End Sub
+    Sub PanelMouseMove()
+        Dim MPos As Integer = Cursor.Position.X
+        Dim dif As Integer = (MPos - StartPos) * -1
+        If mdown Then
+            If dif > 0 Then
+                FlowLayoutPanel1.HorizontalScroll.Value = dif
+            End If
+
+        End If
+        Label3.Text = MPos
+    End Sub
+
+    Sub PanelMouseUp()
+        Mdown = False
+    End Sub
+
+#Region "FlowLayoutPanel"
+
+    ' ************************************************************************************************************************
+    Private Sub FlowLayoutPanel1_MouseDown(sender As Object, e As MouseEventArgs) Handles FlowLayoutPanel1.MouseDown
+        PanelMouseDown()
+
+    End Sub
+    Private Sub FlowLayoutPanel1_MouseMove(sender As Object, e As MouseEventArgs) Handles FlowLayoutPanel1.MouseMove
+        PanelMouseMove()
+    End Sub
+
+    Private Sub FlowLayoutPanel1_MouseUp(sender As Object, e As MouseEventArgs) Handles FlowLayoutPanel1.MouseUp
+        PanelMouseUp()
+    End Sub
+    '*************************************************************************************************************************
+#End Region
+
+
+    Sub LoadLayoutImage(ByVal fname As String, ByVal number As Integer)
+
+        Dim pbx As New PictureBox
+        pbx.SizeMode = PictureBoxSizeMode.StretchImage
+        pbx.BackColor = Color.White
+        pbx.Name = "Picture_" + Trim(Str(number))
+        'pbx.Width = 400
+        'pbx.Height = 266
+        pbx.Width = 350
+        pbx.Height = pbx.Width / 3 * 3
+        pbx.Image = Image.FromFile(fname)
+        AddHandler pbx.Click, AddressOf PictureBox_Click
+        AddHandler pbx.MouseDown, AddressOf PictureBox_MouseDown
+        AddHandler pbx.MouseUp, AddressOf PictureBox_MouseUp
+        AddHandler pbx.MouseMove, AddressOf PictureBox_MouseMove
+        FlowLayoutPanel1.Controls.Add(pbx)
+
+    End Sub
+
+
+
+    Sub GetDirPictures()
+
+        Dim TheFilePath As String
+
+        Dim fbd As New FolderBrowserDialog
+        If fbd.ShowDialog = DialogResult.OK Then
+            TheFilePath = fbd.SelectedPath
+            Try
+                For Each F As String In System.IO.Directory.GetFiles(fbd.SelectedPath)
+                    Dim NewImage As Image = Image.FromFile(F)
+                    NewImage = NewImage.GetThumbnailImage(150, 100, AddressOf Abort, System.IntPtr.Zero)
+                    Dim P As New PictureBox
+                    P.SizeMode = PictureBoxSizeMode.AutoSize
+                    P.Image = NewImage
+                    AddHandler P.Click, AddressOf PictureBox_Click
+                    FlowLayoutPanel1.Controls.Add(P)
+                    ' ListBox1.Items.Add(System.IO.Path.GetFileName(F))
+                Next
+            Catch ex As Exception
+
+            End Try
+
+        End If
+
+    End Sub
+    Sub PictureBox_Click(sender As Object, e As MouseEventArgs)
+
+
+        If StartPos = Lastpos Then
+
+        End If
+
+    End Sub
+    Sub PictureBox_MouseDown(sender As Object, e As MouseEventArgs)
+
+        PanelMouseDown()
+    End Sub
+
+    Sub PictureBox_MouseUp(sender As Object, e As MouseEventArgs)
+        PanelMouseUp()
+
+    End Sub
+
+    Private Sub FlowLayoutPanel1_Paint(sender As Object, e As PaintEventArgs) Handles FlowLayoutPanel1.Paint
+
+    End Sub
+
+    Sub PictureBox_MouseMove(sender As Object, e As MouseEventArgs)
+        PanelMouseMove()
+    End Sub
+    Private Function Abort()
+        Return False
+    End Function
+
+
+
+
+
+End Class
